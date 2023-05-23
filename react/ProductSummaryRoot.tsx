ccb4a67a38573c1ab6f5333a6c0d9f703510c2ff
Bernardo Neves <bernardosbneves@gmail.com>
2023-05-23 10:00:26 -0300

    adding custom components

---
+++ react/ProductSummaryRoot.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import ProductSummary from "./ProductSummary";
+
+const ProductSummaryRoot = () => {
+    return <ProductSummary/>
+}
+
+export default ProductSummaryRoot
